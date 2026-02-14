df7b1c032f3b4af658913bf5252ae919a4f4be92
pydirajuch123 <pydirajuch.tech@gmail.com>
2026-02-14 20:02:52 +0530

    second script

---
+++ 2-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hello $PERSON2, how are you"
+echo "$PERSON2:: hi $PERSON1 ,iam fine thanks for asking,how are doing "
+echo "$PERSON1:: doing fine. what are you learning $PERSON2"
+echo "$PERSON2:: iam learning shell scripting,what about you"
