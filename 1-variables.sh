c8a155f9b216ee62a10cace4ba45b58092bbb5bf
pydirajuch123 <pydirajuch.tech@gmail.com>
2026-02-14 19:57:19 +0530

    second script

--- 1-variables.sh
+++ 1-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=raju
-PERSON2=ramesh
+PERSON1=hari
+PERSON2=sai
 
 echo "$PERSON1:: hello $PERSON2, how are you"
 echo "$PERSON2:: hi $PERSON1 ,iam fine thanks for asking,how are doing "
