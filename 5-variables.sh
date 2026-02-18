f9f6b1736963900f0c31213cadfc4e75f1f90cb8
pydirajuch123 <pydirajuch.tech@gmail.com>
2026-02-18 19:35:13 +0530

    second script

---
+++ 5-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+####special variables####
+echo "all variables passed to script  : $@"
+echo "no of variables passed to script : $#"
+echo "script name : $0"
+echo "present which directory you are in : $PWD"
+echo "who is running thii script : $USER "
+echo "home directory of the user : $HOME"
+echo "pid of this script : $$"
+sleep 10&
